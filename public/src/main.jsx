26e30eae8c93e39af4e235d565923f1b25500d9e
kumi48 <loulounagisa@gmail.com>
2016-12-18 20:39:49 +0900

    kumiko1218

--- public/src/main.jsx
+++ public/src/main.jsx
@@ -24,77 +24,74 @@ class Main extends Component{
     const emitter = new EventEmitter2();
 
     this.state = {
-      emitter,
-      context: null,
-      imageData: null,
-      probs: {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0},
-      result: null,
-      transformed: null
+      emitter
     };
 
-//    const nn = new NN();
-
-    this.state.emitter.on('ctx', (context)=>{
-      //console.log('context',context);
-      const imageData = context.getImageData(0, 244, 1, 1).data;
-      this.setState({
-        context: context,
-        imageData: imageData
-      });
-    }).on('onRecognize', ()=>{
-      //console.log('onRecognize');
-//      const ret = nn.recognize(this.state.context);
-//      this.setState({probs: ret.probs, transformed: ret.img, result: ret.result});
-    }).on('onClear', ()=>{
-      //console.log('onClear');
-      this.state.context.fillStyle = 'white';
-      this.state.context.fillRect(0, 0, 224, 224);
-    }).on('onMouseMove', (event)=>{
-      //console.log('onMouseMove');
-      this.state.context.fillStyle = 'black';
-      this.state.context.fillRect(event.x, event.y, 12, 12);
-      //auto recog?
-//      const ret = nn.recognize(this.state.context);
-//      this.setState({probs: ret.probs, transformed: ret.img, result: ret.result});
-    });
   }
 
   render() {
-    const style = {
-      margin: '0 auto'
+    const tableStyle1 = {
+      border : '1px solid black',
+      borderCollapse: 'collapse',
+      borderColor: 'red',
+      height: '75px',
+      margin: '10 auto',
+      tableLayout: 'fixed',
+      width: '80%'
     };
-    const paperstyle = {
-      height: 224,
-      width: 224,
-      margin: 20,
-      textAlign: 'center',
-      display: 'inline-block',
+    const tableStyle2 = {
+      border : '1px solid black',
+      borderCollapse: 'collapse',
+      borderColor: 'red',
+      height: '400px',
+      margin: '10 auto',
+      tableLayout: 'fixed',
+      width: '80%'
+    };
+    const titleStyle = {
+      color: 'red',
+      fontSize: '30px'
+    };
+    const tdStyle1 = {
+      border : '1px solid black',
+      borderColor: 'orange'
+    };
+    const tdStyle2 = {
+      border : '1px solid black',
+      borderColor: 'orange'
     };
     return (
       <div>
         <div>
-          <table style={style}>
+          <table style={tableStyle1}>
             <tr>
-              <td>
-                <Paper style={paperstyle} zDepth={2} >
-                  <Dcanvas  {...this.state} />
-                </Paper>
+              <td style = {tdStyle1}>
+                Cookie Box
               </td>
-              <td>
-                <Percentage  {...this.state} />
+              <td style = {tdStyle1}>
+                Cookie Community
+              </td>
+              <td style = {tdStyle1}>
+                Search Other Sweets
+              </td>
+              <td style = {tdStyle1}>
+                <FlatButton label="Notification" />
+              </td>
+              <td style = {tdStyle1}>
+                <FlatButton label="Edit Profile" />
+              </td>
+              <td style = {tdStyle1}>
+                <FlatButton label="Logout" />
               </td>
             </tr>
+          </table>
+          <table style={tableStyle2}>
             <tr>
-              <td>
-                <Transformed {...this.state}/>
+              <td style={tdStyle2}>
+                MyList
               </td>
-              <td>
-                <Recognize {...this.state}/>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <Result {...this.state}/>
+              <td style={tdStyle2}>
+                Discovery
               </td>
             </tr>
           </table>
