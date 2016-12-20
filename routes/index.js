daab7607bb9e268f8b2669879ec504a2269e5eae
kani <kani.junya@jp.fujitsu.com>
2016-12-20 13:18:04 +0900

    現時点DBで、idから検索してデータ返すAPIを作成

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,26 @@ pool.getConnection((err, conn)=>{
     console.log(results);
   });
 
+  const getDataById = (conn, target, id)=>{
+    return new Promise((resolve, reject)=>{
+      console.log('getDataById: '+target+',id='+id);
+      const sql = 'SELECT * FROM '+target+' WHERE id=?';
+      if(id){
+        conn.query(sql, id, (err, results)=>{
+          if(err) {
+            reject(err);
+          }
+          else {
+            resolve(results[0]);
+          }
+        });
+      }
+      else{
+        resolve({});
+      }
+    });
+  };
+
   /* GET version. */
   router.get('/', function(req, res, next) {
   //  res.render('index', { title: 'Express' });
@@ -34,33 +54,87 @@ pool.getConnection((err, conn)=>{
 
   /* GET user info */
   router.get('/user', function(req, res, next) {
-    const sql = 'SELECT * FROM user WHERE userId=?';
-    const userId = req.query.userId;
-    if(userId){
-      conn.query(sql, userId, (err, results)=>{
-        if(err) {
-          console.log(err);
-          res.writeHead(400, { 'Content-Type': 'application/json' });
-          res.end(JSON.stringify(err));
-          return;
-        }
-        console.log(results);
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(results[0]));
-      });
-    }
-    else{
+    getDataById(conn, 'user', req.query.id)
+    .then((data)=>{
+      console.log('data',data);
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(data));
+    }).catch((err)=>{
+      console.log('err',err);
       res.writeHead(400, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({}));
-    }
+      res.end(JSON.stringify(err));
+    });
   });
 
-  //booklist
+  /* GET wordbook info */
+  router.get('/wordbook', function(req, res, next) {
+    getDataById(conn, 'wordbook', req.query.id)
+    .then((data)=>{
+      console.log('data',data);
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(data));
+    }).catch((err)=>{
+      console.log('err',err);
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(err));
+    });
+  });
 
-  //wordlist
+  /* GET words info */
+  router.get('/words', function(req, res, next) {
+    getDataById(conn, 'words', req.query.id)
+    .then((data)=>{
+      console.log('data',data);
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(data));
+    }).catch((err)=>{
+      console.log('err',err);
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(err));
+    });
+  });
 
-  //...
+  /* GET category info */
+  router.get('/category', function(req, res, next) {
+    getDataById(conn, 'category', req.query.id)
+    .then((data)=>{
+      console.log('data',data);
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(data));
+    }).catch((err)=>{
+      console.log('err',err);
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(err));
+    });
+  });
 
+  /* GET userScore info */
+  router.get('/userScore', function(req, res, next) {
+    getDataById(conn, 'userScore', req.query.id)
+    .then((data)=>{
+      console.log('data',data);
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(data));
+    }).catch((err)=>{
+      console.log('err',err);
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(err));
+    });
+  });
+
+  /* GET userBookScore info */
+  router.get('/userBookScore', function(req, res, next) {
+    getDataById(conn, 'userBookScore', req.query.id)
+    .then((data)=>{
+      console.log('data',data);
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(data));
+    }).catch((err)=>{
+      console.log('err',err);
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(err));
+    });
+  });
 
 });
 
