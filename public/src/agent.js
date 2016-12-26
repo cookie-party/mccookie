e213b4e4ed125786bbec00c7eb4cebf42222b16f
Junya <kanijunnari@kanijunnari-no-MacBook-Air-2.local>
2016-12-26 20:58:19 +0900

    queryの結果をjsonでもらう

--- public/src/agent.js
+++ public/src/agent.js
@@ -3,13 +3,16 @@ import config from './config/config';
 
 export const query = (target, id)=> {
   return new Promise((resolve, reject)=>{
-    const url = config.HOST || 'http://127.0.0.1:3000/api/';
+    const host = config.HOST || 'localhost';
+    const port = config.PORT || '3000';
+    const version = config.APIVERSION || '1';
+    const url = 'http://'+host+':'+port+'/api/v'+version+'/';
     request
       .get(url+target)
       .query({ id: id }) 
       .end(function(err, res){
         if(err) reject(err);
-        else resolve(res.text);
+        else resolve(JSON.parse(res.text));
       });
   });
 };
