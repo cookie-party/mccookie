5e2ecef12f1986ab39035a260d1a8fa2e3b38c6c
Junya <kanijunnari@kanijunnari-no-MacBook-Air-2.local>
2016-12-26 20:59:01 +0900

    mylistにchartjsを使って表示

---
+++ public/src/mylist.jsx
@@ -0,0 +1,162 @@
+import React, {Component} from 'react';
+import {GridList, GridTile} from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import Subheader from 'material-ui/Subheader';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+import {Pie} from 'react-chartjs-2';
+
+import {query} from './agent';
+
+class MyList extends Component {
+  constructor(props, state){
+    super(props, state);
+
+    this.state = {
+      tilesData :[
+        {
+          key: 1,
+          title: '中学英語１',
+          author: 'kani',
+          atePercentage: 20,
+        },
+        {
+          key: 2,
+          title: '中学歴史１',
+          author: 'murakami',
+          atePercentage: 60,
+        },
+        {
+          key: 3,
+          title: '基本情報',
+          author: 'kumi',
+          atePercentage: 50,
+        }
+      ]
+    };
+  }
+
+  //TODO
+  //1~ => 単語帳
+  //-1 => mylist新規追加
+  onClick(id) {
+    console.log('onClick',id);
+  }
+
+  getBookList() {
+    return new Promise((resolve, reject)=>{
+      query('user', this.props.userId)
+      .then((res)=>{
+        if(res.myBookIdlist){
+          const getBooksQuery = res.myBookIdlist.split(',').map((id)=>{
+            return query('wordbook', id);
+          });
+          Promise.all(getBooksQuery)
+          .then((results)=>{
+            resolve(results);
+          }).catch((err)=>{
+            console.log('query ',err);
+            reject(err);
+          });
+        }else{
+          resolve([]);
+        }
+      }).catch((err)=>{
+        console.log('query ',err);
+        reject(err);
+      });
+    });
+  }
+
+  componentDidMount(){
+    this.getBookList().then((results)=>{
+      //console.log('getBookList',results);
+      if(results.length>0){
+        this.setState({
+          tilesData: results.map((res)=>{
+            return {
+              key: res.id,
+              title: res.name,
+              author: res.createUserId, //TODO ユーザ名にする
+              atePercentage: 20,
+            };
+          })
+        });
+      }
+    });
+  }
+
+  render(){
+    const _calcData = (atePercentage) => {
+      return {
+        labels: [
+          'Ate',
+          'Ever'
+        ],
+        datasets: [{
+          data: [atePercentage, 100-atePercentage],
+          backgroundColor: [
+            '#FF6384',
+            '#36A2EB',
+          ],
+          hoverBackgroundColor: [
+            '#FF6384',
+            '#36A2EB',
+          ]
+        }]
+      };
+    };
+
+    const images = this.state.tilesData.map((tile) => (
+            <GridTile
+              style={{cursor:'pointer'}}
+              key={tile.key}
+              title={tile.title}
+              subtitle={<span>by <b>{tile.author}</b></span>}
+              actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+              onTouchTap={this.onClick.bind(this, tile.key)}
+            >
+              <Pie data={_calcData(tile.atePercentage)} />
+            </GridTile>
+          ));
+
+    images.push((
+      <GridTile
+        style={{cursor:'pointer'}}
+        key={-1}
+        title={'新規追加'}
+        actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+        onTouchTap={this.onClick.bind(this, -1)}
+      >
+        新規追加っぽい画像
+      </GridTile>
+    ));
+
+    const styles = {
+      root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+      },
+      gridList: {
+        display: 'flex',
+        flexWrap: 'nowrap',
+        overflowY: 'auto',
+      },
+    };
+
+    return (
+      <div style={styles.root}>
+        <GridList
+          cellHeight={180}
+          style={styles.gridList}
+        >
+          <Subheader>MyList</Subheader>
+          {images}
+        </GridList>
+      </div>
+    );
+  }
+
+}
+
+export default MyList;
