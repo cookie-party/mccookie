02b3f008dd9e07dc6115dd0e59efc39cf6615508
kani <kani.junya@jp.fujitsu.com>
2017-01-11 09:35:05 +0900

    new list

--- public/src/newlist.jsx
+++ public/src/newlist.jsx
@@ -1,14 +1,53 @@
 import React, {Component} from 'react';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 import {query} from './util/agent';
 
 export default class NewList extends Component {
   constructor(props, state){
     super(props, state);
+    this.state = {
+      name: ''
+    };
   }
+
+  onChangeName(e) {
+    this.setState({name: e.target.value});
+  }
+
+  onClickRegister() {
+    const newlist = {
+      name: this.state.name,
+      wordlist: '',
+      categoryId: '',
+      createUserId: this.props.userId,
+      activate: 1
+    };
+    query('wordbook', newlist)
+    .then((result)=>{
+      console.log(result);
+    }).catch((err)=>{
+      console.log(err);
+    });
+    this.setState({name: ''});
+  }
+
   render() {
+    const styles = {
+      newlist: {
+        display: 'flex',
+      }
+    };
     return (
-      <div>
-        new list
+      <div style={styles.newlist}>
+        <TextField
+          hintText='New list name'
+          value={this.state.key}
+          onChange={this.onChangeName.bind(this)}
+          multiLine={true}
+          rows={1}
+          />
+        <RaisedButton label="登録" primary={true} onClick={this.onClickRegister.bind(this)} />
       </div>
     );
   }
