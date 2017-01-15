eff60e758343b5af85d98b29e27eeb1ab0612f8a
ka2jun8 <narchbald103@gmail.com>
2017-01-16 00:20:12 +0900

    ssl対応

--- public/src/config/config.js
+++ public/src/config/config.js
@@ -5,8 +5,13 @@ if(host.indexOf(':')>0) {
   port = host.substring(host.indexOf(':')+1);
   host = host.substring(0,host.indexOf(':'));
 }
+let ssl = false;
+if(location.href.indexOf('https')>=0){
+  ssl = true;
+}
 module.exports = {
   HOST: host,
   PORT: port,
+  SSL: ssl,
   APIVERSION: '1', //TODO api/versionで取得予定
 };
