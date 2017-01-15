eea518f0c622383d7cd1399ad7e2baadca61afad
ka2jun8 <narchbald103@gmail.com>
2017-01-16 00:08:37 +0900

    configはlocation.hrefから取得

--- public/src/config/config.js
+++ public/src/config/config.js
@@ -1,6 +1,12 @@
 //config
+let host = location.href.substring(location.href.indexOf('//')+2, location.href.length-1);
+let port = '80';
+if(host.indexOf(':')>0) {
+  port = host.substring(host.indexOf(':')+1);
+  host = host.substring(0,host.indexOf(':'));
+}
 module.exports = {
-  HOST: 'mccookietest.herokuapp.com',
-  PORT: '80',
-  APIVERSION: '1',
+  HOST: host,
+  PORT: port,
+  APIVERSION: '1', //TODO api/versionで取得予定
 };
