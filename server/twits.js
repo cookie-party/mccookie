b80706c0a6b8bf78d8819ac4a58b54c3b5e8d526
ka2jun8 <narchbald103@gmail.com>
2017-01-25 00:42:43 +0900

    oauth1.1 に対応

--- server/twits.js
+++ server/twits.js
@@ -1,36 +1,29 @@
 const router = require('express').Router();
-const Twit = require('twit');
+//const Twit = require('twit');
 const OAuth = require('oauth').OAuth;
 
 module.exports = function twitWrapeer(router) {
-  const T = new Twit({
-    consumer_key: process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
-    access_token: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-    timeout_ms: 60*1000,
+
+  /* GET version. */
+  router.get('/twitterWrapper/vesrion', (req, res, next)=> {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ 'twits-version': 'v1' }));
   });
-  
+
   const oa = new OAuth(
     'https://api.twitter.com/oauth/request_token',
     'https://api.twitter.com/oauth/access_token',
     process.env.CONSUMER_KEY, //twitter appで発行されたConsumer keyを入力。
     process.env.CONSUMER_SECRET, //twitter appで発行されたConsumer secretを入力。
-    '1.0',
+    '1.0A',
     'http://127.0.0.1:3000/api/v1/auth/twitter/callback',
     'HMAC-SHA1'
   );
-
-  /* GET version. */
-  router.get('/twits', (req, res, next)=> {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ 'twits-version': 'v1' }));
-  });
-
+  //OAuth 1.0 //passport-twitterにしてもいいかも
   router.get('/auth/twitter', (req, res, next)=> {
     const sess = req.session;
     sess.view = sess.view? sess.view++ : 1;
-    console.log('req.session',sess);
+    //console.log('req.session',sess);
     oa.getOAuthRequestToken((error, oauth_token, oauth_token_secret, results)=>{
       if (error) {
         console.log(error);
@@ -38,18 +31,18 @@ module.exports = function twitWrapeer(router) {
       } else {
         sess.oauth = {};
         sess.oauth.token = oauth_token;
-        console.log('oauth.token: ' + sess.oauth.token);
+        //console.log('oauth.token: ' + sess.oauth.token);
         sess.oauth.token_secret = oauth_token_secret;
-        console.log('oauth.token_secret: ' + sess.oauth.token_secret);
+        //console.log('oauth.token_secret: ' + sess.oauth.token_secret);
         sess.save(()=>{
-          console.log('sess.save req.session',sess);
+          //console.log('sess.save req.session',sess);
           res.redirect('https://twitter.com/oauth/authenticate?oauth_token='+oauth_token);
         });
       }
     });
   });
 
-  router.get('/auth/twitter/callback', function(req, res, next){
+  router.get('/auth/twitter/callback', (req, res, next)=>{
     console.log('callback, session = ', req.session);
     if (req.session.oauth) {
       req.session.oauth.verifier = req.query.oauth_verifier;
@@ -62,7 +55,7 @@ module.exports = function twitWrapeer(router) {
         } else {
           req.session.oauth.access_token = oauth_access_token;
           req.session.oauth.access_token_secret = oauth_access_token_secret;
-          console.log('oauth callback',results);
+          //console.log('oauth callback',results);
           res.send('worked. nice one.');
         }
       });
@@ -71,7 +64,37 @@ module.exports = function twitWrapeer(router) {
     }
   });
 
+  router.get('/twitter/post', (req, res, next)=>{
+    console.log('twitter/post session', req.session);
+    const text = req.query.text || 'oauthでツイートしたった';
+    if(req.session.oauth) {
+      oa.post(
+        'https://api.twitter.com/1.1/statuses/update.json',
+        req.session.oauth.access_token, 
+        req.session.oauth.access_token_secret,
+        {status: text},
+        function (err, data, response) {
+          if (err) {
+            res.send('too bad.' + JSON.stringify(err));
+          } else {
+            res.send('posted successfully...!');
+          }
+        });
+    }
+    else {
+      next(new Error('you\'re not supposed to be here.'));
+    }
+  });
 
+  /*
+  //Twit
+  const T = new Twit({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+    timeout_ms: 60*1000,
+  });
   router.get('/tweets', function(req, res, next) {
     console.log('get tweets', req);
     const q = req.query.q || '#mccookie since:2017-01-01';
@@ -88,6 +111,7 @@ module.exports = function twitWrapeer(router) {
       }
     });
   });
+  */
 
 };
 
